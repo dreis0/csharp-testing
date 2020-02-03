1a73a5ad894346f97dd621e2f85973362106004f
Miguel <migueldreis01@gmail.com>
2020-02-03 18:24:20 -0300

    Parte 3 - Aula 4

---
+++ testing-course/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        ErrorLogger _logger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+
+        /* Testing a void type method */
+
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            string testString = "test";
+            _logger.Log("test");
+
+            Assert.That(_logger.LastError, Is.EqualTo(testString));
+        }
+    }
+}
