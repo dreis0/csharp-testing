0fd42eb841dfcaf9ea5e938137e6469df0c82878
Miguel <migueldreis01@gmail.com>
2020-02-03 18:35:38 -0300

    Parte 3 - Aula 6

--- testing-course/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ testing-course/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -18,7 +18,7 @@ namespace TestNinja.UnitTests
         /* Testing a void type method */
 
         [Test]
-        public void Log_WhenCalled_SetTheLastErrorProperty()
+        public void Log_ValidError_SetTheLastErrorProperty()
         {
             string testString = "test";
             _logger.Log("test");
@@ -37,6 +37,20 @@ namespace TestNinja.UnitTests
             Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
             //For exceptions that are not a property in "Throws" use:
             Assert.That(() => _logger.Log(error), Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+
+        /* Testing method that raise an event */
+
+        [Test]
+        public void Log_ValidError_InvokeEvent()
+        {
+            string testString = "test";
+            var id = Guid.Empty;
+
+            _logger.ErrorLogged += (sender, args) => id = args;
+
+            _logger.Log(testString);
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
         }
     }
 }
