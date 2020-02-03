94454d7aac4e0a410333416ec6b773db7f26d8c1
Miguel <migueldreis01@gmail.com>
2020-02-03 17:53:55 -0300

    Parte 3 - Aula 1

---
+++ testing-course/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        HtmlFormatter _formatter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _formatter = new HtmlFormatter();
+        }
+        public void FormatAsBold_WhenCalled_EncloseTheStringWithStrongElement()
+        {
+            string testString = "test";
+            var result = _formatter.FormatAsBold(testString);
+
+            //Specific
+            Assert.That(result, Is.EqualTo($"<strong>{testString}</strong>"));
+
+            //More general
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain(testString));
+        }
+    }
+}
