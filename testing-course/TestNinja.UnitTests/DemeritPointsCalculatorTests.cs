bc61b37ae3236b661b32de03bc1f0d4e46865eb2
Miguel <migueldreis01@gmail.com>
2020-02-04 17:04:02 -0300

    Parte 4 - Exercício 2

---
+++ testing-course/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        public DemeritPointsCalculator _pointsCalculator { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _pointsCalculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_InvalidSpeed_ThrowsException()
+        {
+            Assert.That(
+                    () => _pointsCalculator.CalculateDemeritPoints(-1), 
+                    Throws.TypeOf<ArgumentOutOfRangeException>()
+                );
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_SpeedBellowLimit_Returns0()
+        {
+            var result = _pointsCalculator.CalculateDemeritPoints(60);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_SpeedAboveLimit_ReturnsDemeritPoints()
+        {
+            var result = _pointsCalculator.CalculateDemeritPoints(85);
+            Assert.That(result, Is.EqualTo(4));
+        }
+    }
+}
