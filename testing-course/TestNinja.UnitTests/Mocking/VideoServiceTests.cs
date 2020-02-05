853bde1a93534af9e7007742d2589482f6aede62
Miguel <migueldreis01@gmail.com>
2020-02-05 16:06:55 -0300

    Aula 5 - Utilizando framework para Mock

--- testing-course/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ testing-course/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,7 +1,6 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
 using TestNinja.Mocking;
 using TestNinja.UnitTests.Mocking.Mocks;
 
@@ -11,18 +10,33 @@ namespace TestNinja.UnitTests.Mocking
     public class VideoServiceTests
     {
         VideoService _videoService;
+        Mock<IFileReader> _fileReader;
 
         [SetUp]
         public void SetUp()
         {
-            _videoService = new VideoService(new FakeFileReader());
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
         }
 
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
             var result = _videoService.ReadVideoTitle();
             Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+
+        [Test]
+        public void ReadVideoTitle_FileHasContent_ReturnsTitle()
+        {
+            _fileReader
+                .Setup(fr => fr.Read("video.txt"))
+                .Returns(JsonConvert.SerializeObject(new Video { Title = "Teste" }));
+
+            var result = _videoService.ReadVideoTitle();
+            Assert.That(result, Is.EqualTo("Teste"));
         }
     }
 }
