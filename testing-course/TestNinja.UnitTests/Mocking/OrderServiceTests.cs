e7e3013958fa79df46a849e279ae079125e08993
Miguel <migueldreis01@gmail.com>
2020-02-05 16:24:00 -0300

    Aula 5 - Teste de interação de classes

---
+++ testing-course/TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        /* Testing the interaction between two classes/objects */
+
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+
+            var order = new Order();
+
+            service.PlaceOrder(order);
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
